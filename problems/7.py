d7037e98c07440766f99de990cf55fd4ed16be3f
ZachAnders <Zacharia.Anders@gmail.com>
2014-02-26 14:45:06 -0700

    problem 7.

--- problems/7.py
+++ problems/7.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env pypy
 
-primes_found, current = 0, 2
+from factor import get_factors
 
+primes_found = 0
+current_prime = 1
 
-#while primes_found < 10000:
-#if 
+while primes_found < 10000:
+	current_prime += 1
+	if len(get_factors(current_prime)) == 1:
+		primes_found += 1
+
+print(current_prime)
